2c96ad23067b5ca8dffaac4e3add6d15dbf535eb
LeoVeig4 <leoveiga2002@gmail.com>
2023-10-10 20:55:39 -0300

    feat - comentary for best explanation

--- tests/test_carrinho.py
+++ tests/test_carrinho.py
@@ -9,7 +9,6 @@ def cart():
     # All setup for the cart here...
     return Carrinho(5)
 
-
 def test_can_add_item_to_cart(cart):
     cart.add("maça")
     assert cart.size() == 1
@@ -18,6 +17,13 @@ def test_can_add_item_to_cart(cart):
 def test_when_item_added_then_cart_contains_item(cart):
     cart.add("maça")
     assert "maça" in cart.get_items()
+
+# def test_when_add_more_than_max_items_should_fail(cart):
+
+#     with pytest.raises(OverflowError):
+#     for _ in range(6):
+#         cart.add("maça")
+
 
 
 def test_when_add_more_than_max_items_should_fail(cart):
